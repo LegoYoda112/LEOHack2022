4fd1cf5101559f49e8d5625f7bdd1ab63d077e33
Thomas G <thomas.godden@outlook.com>
2021-09-22 21:29:03 -0700

    Update base_control.py

--- base_control/base_control.py
+++ base_control/base_control.py
@@ -1,8 +1,16 @@
 import zmq
 
+class SatConnection:
+    def __init__(self, name, ip, port):
+        self.name = name
+        self.ip = ip
+        self.port = port
+
 class BaseControl:
     def __init__(self, name):
         self.name = name
+
+        self.sat = None
 
         self.context = zmq.Context()
 
@@ -33,6 +41,24 @@ class BaseControl:
             return (False, "")
 
         else:
+            print("Sucsessful connection")
 
             sat_name = events[0][0].recv().decode("utf-8")
+
+            self.sat = SatConnection(sat_name, ip, port)
+            print(self.sat)
+
             return (True, sat_name)
+
+        def ping(self):
+            self.send_msg("ping")
+
+            poller = zmq.Poller()
+            poller.register(self.send_socket, zmq.POLLIN)
+
+            events = poller.poll(timeout * 1000)
+
+            print(len(events))
+
+            if(len(events) == 0):
+                print("Connection Failed")
