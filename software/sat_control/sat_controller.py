bd69dc463297c602b0c8a89112c56242fdbd6b7d
LegoYoda112 <thomas.godden@outlook.com>
2021-12-28 21:57:28 -0800

    Updated the sat controller interface

--- software/sat_control/sat_controller.py
+++ software/sat_control/sat_controller.py
@@ -1,6 +1,7 @@
 # Import abstract base class
 from abc import ABC, abstractmethod
 
+
 # Sat controller interface
 import sys
 sys.path.append("../msgs/")
@@ -8,14 +9,35 @@ sys.path.append("software/msgs/")
 
 import sat_descrip_pb2 as sat_msgs
 
-class SatController(ABC):
+# The sat controller class contains all the required information
+# to simulate and control a satalite
+
+class SatControllerInterface(ABC):
+
+    def __init__(self):
+        
+        # Make blank team info
+        self.sat_description = sat_msgs.SatelliteDescription()
+
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'run') and
+                callable(subclass.run) and
+                hasattr(subclass, 'init') and
+                callable(subclass.init))
+
+    def set_mass(self, mass):
+        self.sat_description.mass = new_mass
+    
+    def set_inertia(self, inertia):
+        self.sat_description.inertia = inertia
+
     @abstractmethod
     def run(self, system_state: sat_msgs.SystemState, satellite_state: sat_msgs.SataliteState) -> sat_msgs.ThrustCommand:
-        pass
+        raise NotImplementedError
 
     @abstractmethod
     def init(self) -> sat_msgs.TeamInfo:
-        pass
+        raise NotImplementedError
     
     @abstractmethod
     def reset(self) -> None:
@@ -24,3 +46,45 @@ class SatController(ABC):
     @abstractmethod
     def abort(self) -> None:
         pass
+    
+    # Method to handle system state
+    # Runs a basic state machine
+    def handleState(self, system_state: sat_msgs.SystemState, satellite_state: sat_msgs.SataliteState):
+        state = system_state.state
+        
+        # Basic state machine
+        # Any state transition is allowed
+        # =================== INIT STATE ===================
+        if state == sat_msgs.SystemState.STATE_INIT:
+
+            # Run team init and copy over team info
+            team_info = self.init()
+            self.description.team_info.CopyFrom(team_info)
+
+            # Break
+            break
+
+        # =================== RUN STATE ===================
+        elif state == sat_msgs.SystemState.STATE_RUN:
+
+            # Run team controller and return resulting thrust command
+            thrust_command = self.run(system_state, satellite_state)
+            return thrust_command
+
+            break
+
+        # =================== ABORT STATE ===================
+        elif state == sat_msgs.SystemState.STATE_ABORT:
+
+            # Run team abort code
+            self.abort()
+            break
+
+        # =================== RESET STATE ===================
+        elif state == sat_msgs.SystemState.STATE_RESET:
+
+            # Run team reset code
+            self.reset()
+            break
+        
+        return None
--- software/sat_control/team_controller.py
+++ software/sat_control/team_controller.py
@@ -1,2 +1,36 @@
-import sat_controller
+from sat_controller import SatControllerInterface, sat_msgs
 
+class TeamController(SatControllerInterface):
+
+    def run(self, system_state: sat_msgs.SystemState, satellite_state: sat_msgs.SataliteState) -> sat_msgs.ThrustCommand:
+        
+        # Create a thrust command message
+        thrust_cmd = sat_msgs.ThrustCommand()
+
+        # Set thrust command values
+        thrust_cmd.thrust.f_x = 0.0
+        thrust_cmd.thrust.f_y = 0.0
+        thrust_cmd.thrust.tau = 0.0
+        return thrust_cmd
+
+    def init(self):
+        # Run any initialization you need
+
+        # Update team info
+        team_info = sat_msgs.TeamInfo()
+        team_info.teamName = "Example team"
+        team_info.teamID = 1111
+
+        # Return team info
+        return team_info
+
+    def abort(self) -> None:
+        # Run any abort code
+        pass
+
+    def reset(self) -> None:
+        # Run any reset code
+        pass
+
+test_team_controller = TeamController()
+print(test_team_controller.init())
