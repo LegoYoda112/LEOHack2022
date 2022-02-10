de334206d438ea1460ff4d36752279f240ac6448
Thomas G <thomas.godden@outlook.com>
2022-02-10 11:16:47 +0000

    Added auto host name

--- software/sat_control/run_sat.py
+++ software/sat_control/run_sat.py
@@ -1,4 +1,10 @@
+import socket
 from sat_comms import SatComms
 
-comms = SatComms("test")
+hostname = socket.gethostname()
+
+print(f"Sat control started on {hostname}.local")
+print(socket.gethostbyname(hostname))
+
+comms = SatComms(hostname)
 comms.start("COM2")
