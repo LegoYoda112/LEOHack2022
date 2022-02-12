dff53a14eff08acdcaefc1f662400243f946b53c
NabeelAzuhar <nabeel_azuhar@yahoo.com.sg>
2022-02-12 17:58:50 +0000

    something

--- team/team_controller.py
+++ team/team_controller.py
@@ -58,11 +58,11 @@ class TeamController(SatControllerInterface):
         # Defining target position, damping, k
         x_target = dead_sat_state.pose.x + 0.25 * math.cos(dead_sat_state.pose.theta - math.pi / 2)
         y_target = dead_sat_state.pose.y + 0.25 * math.sin(dead_sat_state.pose.theta - math.pi / 2)
-        k = 20
+        k = 100
         crit_damp = 2 * math.sqrt(self.sat_description.mass * k)
 
         # Set thrust command values, basic PD controller that drives the sat to [0, -1]
-        if self.counter > 1:
+        if self.counter < 2:
             control_message.thrust.f_x = -k * (satellite_state.pose.x - (x_target)) - crit_damp * satellite_state.twist.v_x
             control_message.thrust.f_y = -k * (satellite_state.pose.y - (y_target)) - crit_damp * satellite_state.twist.v_y
             control_message.thrust.tau = -k * (satellite_state.pose.theta - (dead_sat_state.pose.theta) - (math.pi/3)) - crit_damp * satellite_state.twist.omega
