af34999ac3c9786a5a24a263c1fb571578fc428e
NabeelAzuhar <nabeel_azuhar@yahoo.com.sg>
2022-02-12 22:25:56 +0000

    more stuff

--- team/team_controller.py
+++ team/team_controller.py
@@ -57,13 +57,29 @@ class TeamController(SatControllerInterface):
         # Defining target position, damping, k
         x_target = dead_sat_state.pose.x + 0.25 * math.cos(dead_sat_state.pose.theta - math.pi / 2)
         y_target = dead_sat_state.pose.y + 0.25 * math.sin(dead_sat_state.pose.theta - math.pi / 2)
-        k = 1.5
-        crit_damp = 2 * math.sqrt(self.sat_description.mass * k)
+
+        displacement_x = satellite_state.pose.x - dead_sat_state.pose.x
+        displacement_y = satellite_state.pose.y - dead_sat_state.pose.y
+        displacement = math.sqrt(displacement_x ** 2 + displacement_y ** 2)
+
+        if displacement > 0.5:
+            k_x = (math.e/2.03576)**2
+            k_y = (math.e/-0.81430)**2
+        else:
+            k_x = (0.2/(0.5-x_target))**2
+            k_y = (0.2/(0.5-y_target))**2
+
+        crit_damp_x = 2 * math.sqrt(self.sat_description.mass * k_x)
+        crit_damp_y = 2 * math.sqrt(self.sat_description.mass * k_y)
 
         # Set thrust command values, basic PD controller that drives the sat to [0, -1]
-        control_message.thrust.f_x = -k * (satellite_state.pose.x - (x_target)) - crit_damp * satellite_state.twist.v_x
-        control_message.thrust.f_y = -k * (satellite_state.pose.y - (y_target)) - crit_damp * satellite_state.twist.v_y
-        control_message.thrust.tau = -k * (satellite_state.pose.theta - (dead_sat_state.pose.theta) - (math.pi / 3)) - crit_damp * satellite_state.twist.omega
+        control_message.thrust.f_x = -k_x * (satellite_state.pose.x - ((x_target-1.5)*2 + 1.5)) - crit_damp_x * satellite_state.twist.v_x
+        control_message.thrust.f_y = -k_y * (satellite_state.pose.y - ((y_target-(-2))*2 - 2)) - crit_damp_y * satellite_state.twist.v_y
+        control_message.thrust.tau = -k_y * (satellite_state.pose.theta - (dead_sat_state.pose.theta) - (math.pi / 3)) - crit_damp_y * satellite_state.twist.omega
+
+        # control_message.thrust.f_x = -k * (satellite_state.pose.x - (x_target)) - crit_damp * satellite_state.twist.v_x
+        # control_message.thrust.f_y = -k * (satellite_state.pose.y - (y_target)) - crit_damp * satellite_state.twist.v_y
+        # control_message.thrust.tau = -k * (satellite_state.pose.theta - (dead_sat_state.pose.theta) - (math.pi / 3)) - crit_damp * satellite_state.twist.omega
 
         displacement_x = satellite_state.pose.x - dead_sat_state.pose.x
         displacement_y = satellite_state.pose.y - dead_sat_state.pose.y
@@ -75,6 +91,8 @@ class TeamController(SatControllerInterface):
                 print("FAIL")
             else:
                 print("WORKSSS")
+        print("displacement:", displacement)
+        print("velocity:", vel)
 
         # Return control message
         return control_message
